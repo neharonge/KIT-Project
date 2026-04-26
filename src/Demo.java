39a1fe539d8cd74b92329f033e86dc79139db955
neharonge <neharonge2004@gmail.com>
2026-04-26 15:41:16 +0530

    save

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,9 @@
 public class Demo {
     public static void main(String[] args){
         System.out.println("Well-come");
+        test();
+    }
+    public static void test(){
+        System.out.println("Wel-come again");
     }
 }
